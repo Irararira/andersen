7771e6972b265d3826d349c2feba2221e4a617d7
Irina Kirichenko <irinakirichienko@gmail.com>
2021-07-28 16:28:23 +0300

    Ready lesson01

---
+++ src/main/java/com/andersenlab/HomeWorkApp.java
@@ -0,0 +1,12 @@
+package com.andersenlab;
+
+import com.andersenlab.lesson01.Utils;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        Utils.printThreeWords();
+        Utils.checkSumSign();
+        Utils.printColor();
+        Utils.compareNumbers();
+    }
+}
--- src/main/java/com/andersenlab/Main.java
+++
@@ -1,7 +0,0 @@
-package com.andersenlab;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
